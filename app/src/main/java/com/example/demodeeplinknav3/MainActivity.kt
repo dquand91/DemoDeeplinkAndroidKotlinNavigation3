26aece72d8fea0c35cab8bf0ead5df9071738353
Quan Luong <>
2026-01-07 10:54:36 +0700

    Init the Android Kotlin project. Using Kotlin gradle (build.gradle.kts)

---
+++ app/src/main/java/com/example/demodeeplinknav3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.demodeeplinknav3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.demodeeplinknav3.ui.theme.DemoDeepLinkNav3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DemoDeepLinkNav3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemoDeepLinkNav3Theme {
+        Greeting("Android")
+    }
+}
